66c6931620b563249d9e52a540b79b54503ff30d
Alexandr Nebajkin <Alexander.Nebajkin@esterdev.com>
2022-11-21 12:32:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Run this from the working directory where the solution or project to build is located.
+using HostApi;
+
+string[] allfiles = Directory.GetFiles("path/to/dir", "*.*", SearchOption.AllDirectories);
+
+return new DotNetBuild().Build().ExitCode ?? 1;
